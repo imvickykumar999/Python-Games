e4254f0088b2fd59106c16fb3afa540faa381e93
imvickykumar999 <imvickykumar999@gmail.com>
2023-08-04 22:04:20 +0530

    meme

--- Ursina-Engine/Apple-Catcher/apple_catcher.py
+++ Ursina-Engine/Apple-Catcher/apple_catcher.py
@@ -1,10 +1,12 @@
 from ursina import *
 from random import randint
+import time
+
 
 def update():
-    global basket_dx, apple_dy, score
-    basket.x = basket.x + held_keys['right arrow']*time.dt*basket_dx
-    basket.x = basket.x - held_keys['left arrow']*time.dt*basket_dx
+    global newton_dx, apple_dy, score
+    newton.x = newton.x + held_keys['right arrow']*time.dt*newton_dx
+    newton.x = newton.x - held_keys['left arrow']*time.dt*newton_dx
 
     apple.y = apple.y + time.dt* apple_dy
     
@@ -18,21 +20,25 @@ def update():
 
     if apple.y < -4:
         Audio('sounds/lost.wav')
-        print_on_screen("You lose. Let's restart!",position=(0,0),origin=(0,0),scale=2, duration=2)
-        score = 0
-        apple.x = randint(-4,4)
-        apple.y = 10    
+        # print_on_screen("You lose. Let's restart!",position=(0,0),origin=(0,0),scale=2, duration=2)
 
-  
+        video = 'sounds/out.mp4'
+        video_player = Entity(model='quad', parent=camera.ui, scale=(1.8, 1), texture=video, loop=False)
+        video_sound = loader.loadSfx(video)
+        video_player.texture.synchronizeTo(video_sound)
+        video_sound.play()
+        apple.y = 32
+
+
 app=Ursina()
 
 left_wall = Entity(model='quad',color=color.green,scale=(0.6,10),position=(-7,0,0),collider='box')
 right_wall = duplicate(left_wall,x=7)
 
 apple = Entity(model='quad',texture ='img/apple.png',position=(randint(-4,4),10),collider='box')
-basket = Entity(model='quad',texture='img/basket.png',position=(0,-4),collider='box')
+newton = Entity(model='quad',texture='img/isaac-newton.png',position=(0,-4),collider='box')
 
-basket_dx = 5
+newton_dx = 5
 apple_dy = -2
 score = 0
 
